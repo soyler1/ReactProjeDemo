004376980c7c8127e76abe67dda06287807edc61
Kılıçarslan Söyler <kilicarslansoyler3@gmail.com>
2022-09-07 11:56:49 +0300

    NavigationBar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Dashboard from './layouts/Dashboard.jsx'
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
     <div className="App">
-      
+      <Dashboard/>
     </div>
   );
 }
---
+++ src/layouts/Navi.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Menu} from 'semantic-ui-react'
+
+export default function Navi() {
+  return (
+    <div>
+        <Menu>
+        <Menu.Item name='browse'>Browse</Menu.Item>
+
+        <Menu.Item name='submit'>Submit</Menu.Item>
+
+        <Menu.Menu position='right'>
+          <Menu.Item name='signup'>Sign Up</Menu.Item>
+
+          <Menu.Item name='help'>Help</Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    </div>
+  )
+}
